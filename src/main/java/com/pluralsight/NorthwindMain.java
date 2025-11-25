6694651ac11874865e3587fba868a6c2d90aa54f
Nauman <naumansqb@gmail.com>
2025-11-25 11:04:31 -0500

    closed properly and used table formatting

--- src/main/java/com/pluralsight/NorthwindMain.java
+++ src/main/java/com/pluralsight/NorthwindMain.java
@@ -18,17 +18,24 @@ public class NorthwindMain {
             PreparedStatement preparedStatement = connection.prepareStatement(query);
 
             ResultSet results= preparedStatement.executeQuery();
+            System.out.printf("%-4s %-40s %-8s %s","ID","Name","Price","Stock");
+            System.out.println();
+            System.out.println("-".repeat(80));
             while(results.next()){
                 int productID= results.getInt(1);
                 String productName = results.getString(2);
                 double unitPrice = results.getDouble(3);
                 int unitsInStock = results.getInt(4);
-                System.out.println("Product ID : "+productID);
+                System.out.printf("%-4d %-40s %-8.2f %d %n",productID,productName,unitPrice,unitsInStock);
+/*                System.out.println("Product ID : "+productID);
                 System.out.println("Name : "+productName);
                 System.out.println("Price: "+unitPrice);
                 System.out.println("Stock : "+unitsInStock);
-                System.out.println("================================");
+                System.out.println("================================");*/
             }
+            results.close();
+            preparedStatement.close();
+            connection.close();
         } catch (Exception e) {
             System.out.println("An error has occured " + e);
             e.printStackTrace();
